d145527d36fa8171872bae5732c92a283b49459a
seniorcub <farinderif@gmail.com>
2024-07-04 18:19:18 +0100

    chore: Update server.js imports and remove unused code

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 import express from 'express';
 import path from 'path';
-import bcrypt from 'bcrypt';
 import dotenv from 'dotenv';
 import connectDB from './dB/connect.js';
-import colors from 'colors'; // Fixed import name to match npm package
 import morgan from 'morgan';
-import usermodels from './models/usermodels.js';
 import authroute from './routes/authroute.js';
-import facultymodels from './models/facultymodels.js';
 import facultyroute from './routes/facultyroute.js';
 
 dotenv.config();
@@ -31,5 +27,5 @@ app.use((req, res, next) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`.bgBlue.red);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
