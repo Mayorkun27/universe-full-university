bda444c6aab6feb0e08ef0688eda38d259a8450c
seniorcub <farinderif@gmail.com>
2024-07-04 20:26:48 +0100

    Update routes to use multer middleware for file uploads

--- api/api.js
+++ api/api.js
@@ -17,7 +17,6 @@ app.use(express.json());
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-app.use('/', express.static(path.join(process.cwd(), 'public')));
 app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
 
 app.use('/api/v1/auth', authroute);
@@ -25,9 +24,11 @@ app.use('/api/v1/faculty', facultyroute);
 
 // Middleware for handling 404 errors
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(process.cwd(), 'View/404.html'));
+    res.status(404).send('404 Not Found');
 });
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
+
+export default app;
