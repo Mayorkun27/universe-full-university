76055b2e34de0f8fd07effe005b4eaa1ce1f99e6
seniorcub <farinderif@gmail.com>
2024-07-04 20:16:39 +0100

    Update routes to use multer middleware for file uploads
    This commit modifies the authroute.js and facultyroute.js files to include the multer middleware for handling file uploads. It adds the necessary code to configure multer, including setting the destination folder and generating unique filenames. This change enhances the functionality of the routes by allowing clients to upload files, such as profile pictures, during the registration process.

--- routes/authroute.js
+++ routes/authroute.js
@@ -1,11 +1,38 @@
-import express from 'express';
+import express from "express";
+import multer from 'multer';
+import path from 'path';
+import cors from 'cors';
 import { registerClient, loginClient, getUsers, deleteUser } from '../controller/authcontroller.js';
+import { isAdmin, signinController } from "../middlewares/signinController.js";
 
 const router = express.Router();
 
-router.post('/register', registerClient);
+const storage = multer.diskStorage({
+     destination(req, file, cb) {
+          cb(null, 'uploads/');
+     },
+     filename(req, file, cb) {
+          cb(null, Date.now() + path.extname(file.originalname));
+     }
+});
+
+const upload = multer({ storage: storage });
+
+router.use('/uploads', express.static('uploads'));
+
+router.use(cors());
+
+router.use(express.json());
+router.use(express.urlencoded({ extended: false }));
+
+router.post('/signUp', upload.single('photo'), registerClient); // Use multer middleware here
+
 router.post('/login', loginClient);
+
 router.get('/users', getUsers);
-router.delete('/delete/:id', deleteUser);
+
+router.get('/user', signinController, isAdmin);
+
+router.delete("/users/:id", deleteUser); 
 
 export default router;
