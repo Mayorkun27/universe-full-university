2284272212750044df854e2273b5df982ccae4fc
seniorcub <farinderif@gmail.com>
2024-07-04 18:33:16 +0100

    Update server.js to enable CORS and add routes for /api/v1/auth and /api/v1/faculty

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ const PORT = process.env.PORT || 2111;
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 app.use('/', express.static(path.join(process.cwd(), 'public')));
 app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
 
-app.use(cors());
 app.use('/api/v1/auth', authroute);
 app.use('/api/v1/faculty', facultyroute);
 
