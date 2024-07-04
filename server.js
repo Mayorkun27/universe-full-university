83cfdf139633a42ccd7bc4a10f29bc23ac429d31
seniorcub <farinderif@gmail.com>
2024-07-04 21:18:55 +0100

    Update routes to use multer middleware for file uploads

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import path from 'path';
+import bcrypt from 'bcrypt';
 import dotenv from 'dotenv';
 import connectDB from './dB/connect.js';
+import colors from 'colors'; // Fixed import name to match npm package
 import morgan from 'morgan';
+import usermodels from './models/usermodels.js';
 import authroute from './routes/authroute.js';
+import facultymodels from './models/facultymodels.js';
 import facultyroute from './routes/facultyroute.js';
-import cors from 'cors';
 
 dotenv.config();
 connectDB();
@@ -16,7 +19,7 @@ const PORT = process.env.PORT || 2111;
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use('/', express.static(path.join(process.cwd(), 'public')));
 app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
 
 app.use('/api/v1/auth', authroute);
@@ -24,11 +27,9 @@ app.use('/api/v1/faculty', facultyroute);
 
 // Middleware for handling 404 errors
 app.use((req, res, next) => {
-    res.status(404).send('404 Not Found');
+    res.status(404).sendFile(path.join(process.cwd(), 'View/404.html'));
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`.bgBlue.red);
 });
-
-export default app;
