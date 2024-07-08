abde876ff200d2cce4eaea0a986dc5a315fc6df7
Mayorkun27 <156526127+Mayorkun27@users.noreply.github.com>
2024-07-08 03:02:23 +0100

    Finished from chancellor page alongside alumni

--- server.js
+++ server.js
@@ -19,16 +19,18 @@ const PORT = process.env.PORT || 2111;
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
-app.use('/', express.static(path.join(process.cwd(), 'public')));
-app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
 
-app.use('/api/v1/auth', authroute);
-app.use('/api/v1/faculty', facultyroute);
+app.use('/', express.static(path.join(process.cwd(), 'public')));
 
 // Middleware for handling 404 errors
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(process.cwd(), 'View/404.html'));
 });
+app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
+
+app.use('/api/v1/auth', authroute);
+app.use('/api/v1/faculty', facultyroute);
+
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`.bgBlue.red);
